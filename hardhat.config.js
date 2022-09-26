4deebdb57fe7933daa8bb0f0ba2c94c45b735c62
haseebullahabbasi <haseebabbasi00@gmail.com>
2022-09-27 02:54:11 +0500

    deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,15 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-waffle");
-const GOERLI_HTTPS =
-  "https://eth-goerli.g.alchemy.com/v2/Z62cFUE0R-m7keWssHy7NE4aCV-J7iuu";
-
-const GOERLI_API_KEY = "Z62cFUE0R-m7keWssHy7NE4aCV-J7iuu";
+const GOERLI_API_KEY = "_1AZv8ynIzDmSuSS2P0GTOXoJy5iyGq9";
 const GOERLI_PVT_API_KEY =
   "8afb0a413215c882358fa599f2c1bc6a055b45d8435305758eccc98eae5151d4";
 
+const GOERLI_HTTPS = `https://eth-goerli.g.alchemy.com/v2/${GOERLI_API_KEY}`;
 module.exports = {
   solidity: "0.8.9",
   networks: {
     goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/${GOERLI_API_KEY}`,
+      url: GOERLI_HTTPS,
       accounts: [`0x${GOERLI_PVT_API_KEY}`],
     },
   },
