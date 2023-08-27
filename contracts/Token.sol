eaed67f49f39149760d061bf3650c70e2b25c216
Haseeb Ullah <53255366+HaseebUllahAbbasi@users.noreply.github.com>
2023-08-27 20:16:28 +0500

    supply changes

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.5.0 <0.9.0;
 contract Token {
     string public name = "Dot Token";
     string public symbol = "DOT";
-    uint256 public totalSupply = 10000;
+    uint256 public totalSupply = 10000000;
 
     address public owner;
     mapping(address => uint256) balances;
